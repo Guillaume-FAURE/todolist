b3d6093ad17631f861d42d091e0b9a55f400009a
Guillaume-FAURE <faureg0717@gmail.com>
2022-02-28 18:55:07 +0100

    feat(components):add button.js update task.js remove auto fill by next

--- components/task.js
+++ components/task.js
@@ -1,20 +1,41 @@
 import styles from './task.module.css'
+import styled from 'styled-components'
+import {AddButton,EditButton} from './button'
 
-function Task ({children}, {props}){
+const BorderLeftSuperCase = styled.div`
+    width: 2px;
+    height: auto;
+    box-shadow: 3px 0px 3px ${props=>props.theme.color};
+    display: flex;
+`
+const Priority = styled.div`
+    width:30px;
+    height:30px;
+    background-color:${props=> props.theme.color};
+    border-radius:30px;
+    display:flex;
+    box-shadow: 0px 0px 2px grey;
+`
+
+function Task ({children}){
     return (
         <div className={styles.superTaskCase}>
-            <div className={styles.taskCase}>
-                <div className={styles.priorityCase}>
-
+            <BorderLeftSuperCase theme={{color:"red"}}></BorderLeftSuperCase>
+            <div className={styles.contentSuperCase}>
+                <div className={styles.taskCase}>
+                    <div className={styles.taskColumn1}>
+                        <Priority theme={{ color:"red"}}></Priority>
+                    </div>
+                    <div className={styles.taskColumn2}>
+                        <textarea className={styles.input} autoFocus></textarea>
+                    </div>
+                    <div className={styles.taskColumn3}>
+                        <AddButton></AddButton>
+                        <EditButton></EditButton>
+                    </div>
                 </div>
-                <div className={styles.inputCase}>
-                    <input type='text' autoFocus></input>
-                </div>
-                <div className={styles.addSubTaskCase}>
-
-                </div>
+                {children}
             </div>
-        {children}
         </div>
     )
 }
--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import styles from '../styles/home.module.css'
 import Task from '../components/task'
 
 
@@ -14,54 +12,20 @@ function Home() {
         <meta name="author" content="Guillaume-FAURE" />
         <meta name="description" content="ToDo list app with local storage of information created with next.js" />
         <meta  charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="" />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
           ToDoList App
         </h1>
-        <h1 className={styles.title}>
-          <Link href="/settings">
-            <a>Go to settings</a>
-          </Link>
-        </h1>
-        <div className={`${styles.addButton} ${styles.mainButton}`}>
-        </div>
-        <div className={styles.todolist}>
-          <a href="https://nextjs.org/docs" className={styles.todoContainer}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.todoContainer}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.todoContainer}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.todoContainer}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-        <Task>
-          <Task></Task>
+        <div className={styles.tasks}>
           <Task>
-            <Task></Task>  
+            <Task></Task>
+            <Task>
+              <Task></Task>  
+            </Task>
           </Task>
-        </Task>
+        </div>
       </main>
       <footer className={styles.footer}>
       </footer>
